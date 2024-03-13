bd8f5464bf41fc54eeaf8ede4e9f81353064dd70
naidk <80177026+naidk@users.noreply.github.com>
2024-03-13 18:20:16 +0530

    Update python_dependencies.sh

--- dev/crm/scripts/python_dependencies.sh
+++ dev/crm/scripts/python_dependencies.sh
@@ -2,4 +2,4 @@
 
 virtualenv /home/ubuntu/env
 source /home/ubuntu/env/bin/activate
-pip install -r /home/ubuntu/Django-CRM-mastery-app-Project-1-YT-main/dev/requirements.txt
+pip install -r /home/ubuntu/Django-crm/dev/requirements.txt
